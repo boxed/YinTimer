30d2039e39d00fa614cf3b56e86e9cb533d37cfe
Anders Hovmöller <boxed@killingar.net>
2020-07-11 21:53:23 +0200

    Initial Commit

---
+++ YinTimer/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  YinTimer
+//
+//  Created by Anders Hovmöller on 2020-07-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YinTimer/YinTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  YinTimerApp.swift
+//  YinTimer
+//
+//  Created by Anders Hovmöller on 2020-07-11.
+//
+
+import SwiftUI
+
+@main
+struct YinTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
