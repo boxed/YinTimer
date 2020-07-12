9ead93f22e11ffe6b7af8dbd4a0094bc5d334980
Anders Hovmöller <boxed@killingar.net>
2020-07-12 10:01:58 +0200

    App is complete-ish

--- YinTimer/ContentView.swift
+++ YinTimer/ContentView.swift
@@ -15,56 +15,70 @@ let presets = [
 
 
 struct ContentView: View {
-    @State var times: (Date, Date) = (Date(), Date.init(timeIntervalSinceNow: 5))
+    @State var times: (Date, Date) = (Date.init(timeIntervalSince1970: 0), Date.init(timeIntervalSince1970: 0))
     
     @State var now: Date = Date()
     
     let timer = Timer.publish(every: 0.05, on: .main, in: .common).autoconnect()
     
-    var body: some View {
-        Path { path in
-            let (start, stop) = times
-            if now <= stop {
-                let length = stop.distance(to: start)
-                let progress = length / now.distance(to: stop)
-                let pi = Double.pi
-                let position: Double = pi - (2*pi / progress)
-                let size = UIScreen.main.bounds.height
-                let x = UIScreen.main.bounds.midX + CGFloat(sin(position)) * size
-                let y = UIScreen.main.bounds.midY + CGFloat(cos(position)) * size
-                path.move(to: CGPoint(x: UIScreen.main.bounds.midX, y: UIScreen.main.bounds.midY))
-                path.addLine(to: CGPoint(x: x, y: y))
+    var clock: some View {
+        VStack {
+            Path { path in
+                let (start, stop) = times
+                if now <= stop {
+                    let length = stop.distance(to: start)
+                    let progress = length / now.distance(to: stop)
+                    let pi = Double.pi
+                    let position: Double = pi - (2*pi / progress)
+                    let size = UIScreen.main.bounds.height
+                    let offset: CGFloat = UIScreen.main.bounds.height * 0.075
+                    let x = UIScreen.main.bounds.midX + CGFloat(sin(position)) * size
+                    let y = UIScreen.main.bounds.midY + CGFloat(cos(position)) * size
+                    path.move(
+                        to: CGPoint(
+                            x: UIScreen.main.bounds.midX,
+                            y: UIScreen.main.bounds.midY - offset
+                        )
+                    )
+                    path.addLine(to: CGPoint(x: x, y: y - offset))
+                }
             }
+            .stroke(Color.white, style: StrokeStyle(lineWidth: 8, lineCap:.round))
+            .onReceive(timer) { input in
+                now = input
+            }
+            .background(Rectangle().fill(Color.black)).foregroundColor(Color.white)
         }
-        .stroke(Color.white, lineWidth: 4)
-        .onReceive(timer) { input in
-            now = input
+    }
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 10) {
+            Spacer().frame(maxWidth: .infinity)
+            HStack {
+                ForEach(presets.indices, id: \.self) { i in
+                    Button(action: {
+                        times = (
+                            Date.init(),
+                            Date.init(timeIntervalSinceNow: TimeInterval(60 * presets[i])))
+                    }) {
+                        Text("\(presets[i])")
+                        .font(.system(size: 20))
+                        .foregroundColor(.white)
+                    }.padding()
+                }
+                Button(action: {
+                    times = (Date.init(timeIntervalSince1970: 0), Date.init(timeIntervalSince1970: 0))
+                })  {
+                    Text("Stop")
+                    .font(.system(size: 20))
+                    .foregroundColor(.white)
+                }.padding()
+            }
+            Spacer().frame(maxHeight: 5)
         }
         .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
-        .background(Rectangle().fill(Color.black)).foregroundColor(Color.white)
-//        if times != nil {
-//            return Clock()
-//                .stroke(lineWidth: 8)
-//                .frame(width: UIScreen.main.bounds.height, height: UIScreen.main.bounds.height)
-//                .foregroundColor(.purple)
-
-//        }
-//        else {
-//            return VStack {
-//                ForEach(presets.indices, id: \.self) { i in
-//                    Button(action: {
-//                        times = (
-//                            Date.init(),
-//                            Date.init(timeIntervalSinceNow: TimeInterval(60 * presets[i])))
-//                    }) {
-//                        Text("\(presets[i])m")
-//                            .font(.system(size: 20))
-//                    }
-//                }
-//            }
-//        }
+        .background(clock)
         .preferredColorScheme(.dark)
-        .statusBar(hidden: /*@START_MENU_TOKEN@*/false/*@END_MENU_TOKEN@*/)
     }
 }
 
