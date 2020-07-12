3f774199f456c20a7b14ff63b7439ddaae5db1c4
Anders Hovmöller <boxed@killingar.net>
2020-07-12 21:56:25 +0200

    Implemented settings

--- YinTimer/ContentView.swift
+++ YinTimer/ContentView.swift
@@ -7,20 +7,25 @@
 
 import SwiftUI
 
-let presets = [
-    3,
-    5,
-    6,
-]
+let choices: [Int] = [Int](1...20) + stride(from: 25, to: 95, by: 5)
 
 let epoch = (Date.init(timeIntervalSince1970: 0), Date.init(timeIntervalSince1970: 0))
 
+func readPresets() -> [Int] {
+    UserDefaults.standard.register(defaults: ["presets": [3, 5, 6]])
+    return UserDefaults.standard.object(forKey: "presets") as! [Int]
+}
+
+func savePresets(_ presets: [Int]) {
+    UserDefaults.standard.setValue(presets, forKey: "presets")
+}
 
 struct ContentView: View {
     @State var times: (Date, Date) = epoch
-    
     @State var now: Date = Date()
-    
+    @State var showSettings = false
+    @State var presets = readPresets()
+
     let timer = Timer.publish(every: 0.05, on: .main, in: .common).autoconnect()
     
     var clock: some View {
@@ -33,7 +38,7 @@ struct ContentView: View {
                     let pi = Double.pi
                     let position: Double = pi - (2*pi / progress)
                     let size = UIScreen.main.bounds.height
-                    let offset: CGFloat = UIScreen.main.bounds.height * 0.075
+                    let offset: CGFloat = UIScreen.main.bounds.height * 0.070
                     let x = UIScreen.main.bounds.midX + CGFloat(sin(position)) * size
                     let y = UIScreen.main.bounds.midY + CGFloat(cos(position)) * size
                     path.move(
@@ -53,30 +58,74 @@ struct ContentView: View {
         }
     }
     
-    var body: some View {
-        VStack(alignment: .center, spacing: 10) {
+    var settings: some View {
+        HStack {
             Spacer().frame(maxWidth: .infinity)
-            HStack {
-                ForEach(presets.indices, id: \.self) { i in
-                    Button(action: {
-                        times = (
-                            Date.init(),
-                            Date.init(timeIntervalSinceNow: TimeInterval(60 * presets[i])))
-                        UIApplication.shared.isIdleTimerDisabled = true
-                    }) {
-                        Text("\(presets[i])")
-                        .font(.system(size: 20))
-                        .foregroundColor(.white)
-                    }.padding()
+            if showSettings {
+                ScrollView(.vertical, showsIndicators: true) {
+                    VStack {
+                        ForEach(choices, id: \.self) { choice in
+                            Button(action: {
+                                var s = Set<Int>.init(presets)
+                                if !presets.contains(choice) {
+                                    s.insert(choice)
+                                }
+                                else {
+                                    s.remove(choice)
+                                }
+                                presets = s.sorted()
+                                savePresets(presets)
+                            }) {
+                                Text("\(choice)")
+                                .font(.system(size: 20))
+                                .foregroundColor(presets.contains(choice) ? .white : .gray)
+                            }.padding()
+                        }
+                    }
                 }
+            }
+            else {
+                ScrollView {}
+            }
+        }
+    }
+    
+    var presetsButtons: some View {
+        HStack {
+            ForEach(presets.indices, id: \.self) { i in
                 Button(action: {
-                    times = epoch
-                })  {
-                    Text("Stop")
+                    times = (
+                        Date.init(),
+                        Date.init(timeIntervalSinceNow: TimeInterval(60 * presets[i])))
+                    UIApplication.shared.isIdleTimerDisabled = true
+                }) {
+                    Text("\(presets[i])")
                     .font(.system(size: 20))
                     .foregroundColor(.white)
                 }.padding()
             }
+            Button(action: {
+                times = epoch
+            })  {
+                Text("Stop")
+                .font(.system(size: 20))
+                .foregroundColor(.white)
+            }.padding()
+        }
+    }
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 10) {
+            HStack {
+                Spacer().frame(maxWidth: .infinity)
+                Button(action: {
+                    showSettings.toggle()
+                }) {
+                    Image(systemName: "gear")
+                }.padding().foregroundColor(.white)
+            }
+            settings
+            presetsButtons
             Spacer().frame(maxHeight: 5)
         }
         .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
