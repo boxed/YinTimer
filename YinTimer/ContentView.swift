af5d562271689f14d7d040b277510ac214c0b327
Anders Hovmöller <boxed@killingar.net>
2020-11-21 22:07:57 +0100

    Version 2! Songs from iCloud Drive.

--- YinTimer/ContentView.swift
+++ YinTimer/ContentView.swift
@@ -6,8 +6,14 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 let choices: [Int] = [Int](1...20) + stride(from: 25, to: 95, by: 5)
+
+struct Song : Equatable {
+    let fileURL: URL
+    let symbol: String
+}
 
 let epoch = (Date.init(timeIntervalSince1970: 0), Date.init(timeIntervalSince1970: 0))
 
@@ -20,36 +26,88 @@ func savePresets(_ presets: [Int]) {
     UserDefaults.standard.setValue(presets, forKey: "presets")
 }
 
+func readSongs() -> [Song] {
+    let fm = FileManager.default
+    // TODO: do this in a queue! https://dev.to/nemecek_f/ios-saving-files-into-user-s-icloud-drive-using-filemanager-4kpm
+    guard let driveURL = fm.url(forUbiquityContainerIdentifier: nil)?.appendingPathComponent("Documents") else { return []
+    }
+    
+    do {
+        try fm.createDirectory(at: driveURL, withIntermediateDirectories: true, attributes: nil)
+        let howto = driveURL.appendingPathComponent("how to add songs.txt").path
+        if !fm.fileExists(atPath: howto) {
+            fm.createFile(atPath: howto, contents: """
+            How to add songs to YinTimer
+            ============================
+
+            Add .mp3 files in this folder. Follow this pattern:
+
+            01 ♫ anything you want.mp3
+            02 🎸 anything you want.mp3
+
+            The numbers in the front are for making the songs appear in the order you want in YinTimer. Then you should have a space, followed by a symbol that is shown on the button. This can be any text you want. Anything after that is ignored.
+            """.data(using: .utf8), attributes: nil)
+        }
+        return try fm.contentsOfDirectory(at: driveURL, includingPropertiesForKeys: [.isDirectoryKey], options: .skipsHiddenFiles).map { url -> Song? in
+            let filename = url.lastPathComponent
+            if !filename.hasSuffix(".mp3") {
+                return nil
+            }
+            let parts = filename.split(separator: " ", maxSplits: 1)
+            if parts.count != 2 || parts[1].count < 1 {
+                return nil
+            }
+            guard let symbol = String(parts[1]).first else { return nil }
+            return Song(fileURL: url, symbol: String(symbol))
+        }.compactMap { $0 }
+    }
+    catch {
+        return []
+    }
+}
+
+func clockPathInner(path: inout Path, bounds: CGRect, progress: TimeInterval) {
+    let pi = Double.pi
+    let position: Double = pi - (2*pi / progress)
+    let size = bounds.height
+    let offset: CGFloat = bounds.height * 0.070
+    let x = bounds.midX + CGFloat(sin(position)) * size
+    let y = bounds.midY + CGFloat(cos(position)) * size
+    path.move(
+        to: CGPoint(
+            x: bounds.midX,
+            y: bounds.midY - offset
+        )
+    )
+    path.addLine(to: CGPoint(x: x, y: y - offset))
+
+}
+
+func clockPath(times: (Date, Date), now: Date, bounds: CGRect) -> Path {
+    Path { path in
+        let (start, stop) = times
+        if now <= stop {
+            let length = stop.distance(to: start)
+            let progress = length / now.distance(to: stop)
+            clockPathInner(path: &path, bounds: bounds, progress: progress)
+        }
+    }
+}
+
 struct ContentView: View {
     @State var times: (Date, Date) = epoch
     @State var now: Date = Date()
     @State var showSettings = false
     @State var presets = readPresets()
+    @State var songs = readSongs()
+    @State var player: AVAudioPlayer? = nil
+    @State var currentSong: Song? = nil
 
     let timer = Timer.publish(every: 0.05, on: .main, in: .common).autoconnect()
     
     var clock: some View {
         VStack {
-            Path { path in
-                let (start, stop) = times
-                if now <= stop {
-                    let length = stop.distance(to: start)
-                    let progress = length / now.distance(to: stop)
-                    let pi = Double.pi
-                    let position: Double = pi - (2*pi / progress)
-                    let size = UIScreen.main.bounds.height
-                    let offset: CGFloat = UIScreen.main.bounds.height * 0.070
-                    let x = UIScreen.main.bounds.midX + CGFloat(sin(position)) * size
-                    let y = UIScreen.main.bounds.midY + CGFloat(cos(position)) * size
-                    path.move(
-                        to: CGPoint(
-                            x: UIScreen.main.bounds.midX,
-                            y: UIScreen.main.bounds.midY - offset
-                        )
-                    )
-                    path.addLine(to: CGPoint(x: x, y: y - offset))
-                }
-            }
+            clockPath(times: times, now: now, bounds: UIScreen.main.bounds)
             .stroke(Color.white, style: StrokeStyle(lineWidth: 8, lineCap:.round))
             .onReceive(timer) { input in
                 now = input
@@ -91,27 +149,79 @@ struct ContentView: View {
     }
     
     var presetsButtons: some View {
-        HStack {
-            ForEach(presets.indices, id: \.self) { i in
+        ScrollView(.horizontal) {
+            HStack {
+                ForEach(presets.indices, id: \.self) { i in
+                    Button(action: {
+                        times = (
+                            Date.init(),
+                            Date.init(timeIntervalSinceNow: TimeInterval(60 * presets[i])))
+                        UIApplication.shared.isIdleTimerDisabled = true
+                    }) {
+                        Text("\(presets[i])")
+                        .font(.system(size: 20))
+                        .foregroundColor(.white)
+                    }.padding()
+                }
                 Button(action: {
-                    times = (
-                        Date.init(),
-                        Date.init(timeIntervalSinceNow: TimeInterval(60 * presets[i])))
-                    UIApplication.shared.isIdleTimerDisabled = true
-                }) {
-                    Text("\(presets[i])")
+                    times = epoch
+                })  {
+                    Text("Stop")
                     .font(.system(size: 20))
                     .foregroundColor(.white)
                 }.padding()
             }
-            Button(action: {
-                times = epoch
-            })  {
-                Text("Stop")
-                .font(.system(size: 20))
-                .foregroundColor(.white)
-            }.padding()
         }
+    }
+  
+    var songButtons: some View {
+        ScrollView(.horizontal) {
+            HStack {
+                ForEach(songs.indices, id: \.self) { i in
+                    Button(action: {
+                        // TODO: start/stop song
+                        do {
+                            if player != nil {
+                                player?.stop()
+                                player = nil
+                                if songs[i] == currentSong {
+                                    return
+                                }
+                            }
+                            player = try AVAudioPlayer(contentsOf: songs[i].fileURL)
+                            player?.prepareToPlay()
+                            player?.numberOfLoops = -1
+                            try AVAudioSession.sharedInstance().setCategory(.playback)
+                            player?.play()
+                            currentSong = songs[i]
+                        }
+                        catch {
+                        }
+                    }) {
+                        Text("\(songs[i].symbol)")
+                        .font(.system(size: 20))
+                        .foregroundColor(.white)
+                        .frame(width: 50, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .background(
+                            Circle()
+                            .strokeBorder()
+                            .foregroundColor(player != nil && songs[i] == currentSong ? .purple : .white  )
+                        )
+                        // TODO: progress information for player
+//                        .background(
+//                            Path { path in
+//                                if player != nil && songs[i] == currentSong {
+//                                    if let currentTime = player?.currentTime, let duration = player?.duration {
+//                                        clockPathInner(path: &path, bounds: path.boundingRect, progress: currentTime/duration)
+//                                    }
+//                                }
+//                            }
+//                        )
+                    }
+                }
+            }
+        }
+        .padding(EdgeInsets(top: 0, leading: 5, bottom: 0, trailing: 5))
     }
     
     var body: some View {
@@ -126,6 +236,7 @@ struct ContentView: View {
             }
             settings
             presetsButtons
+            songButtons
             Spacer().frame(maxHeight: 5)
         }
         .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
