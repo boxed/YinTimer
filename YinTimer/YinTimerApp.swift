14e538b04f2550b3329d7bc96520b3cf78f875eb
Anders Hovmöller <anders.hovmoller@dryft.se>
2025-08-19 14:39:43 +0200

    Fix for preventing screen lock

--- YinTimer/YinTimerApp.swift
+++ YinTimer/YinTimerApp.swift
@@ -9,9 +9,21 @@ import SwiftUI
 
 @main
 struct YinTimerApp: App {
+    @Environment(\.scenePhase) var scenePhase
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+        .onChange(of: scenePhase) { (phase) in
+            switch phase {
+            case .active:
+                // In iOS 13+, idle timer needs to be set in scene to override default
+                UIApplication.shared.isIdleTimerDisabled = true
+            case .inactive: break
+            case .background: break
+            @unknown default: print("ScenePhase: unexpected state")
+            }
         }
     }
 }
